0da7aab9f7de2d380929f83ac91924c9e35b2686
phucvin <phucvin@gmail.com>
2022-11-23 02:33:28 +0000

    gofmt

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,44 +1,44 @@
 package main
 
 import (
-    "context"
-    "log"
-    "net/http"
+	"context"
+	"log"
+	"net/http"
 
 	"google.golang.org/protobuf/types/known/anypb"
 
-    greetv1 "testbufconnect/gen/greet/v1"
-    "testbufconnect/gen/greet/v1/greetv1connect"
+	greetv1 "testbufconnect/gen/greet/v1"
+	"testbufconnect/gen/greet/v1/greetv1connect"
 
-    "github.com/bufbuild/connect-go"
+	"github.com/bufbuild/connect-go"
 )
 
 func main() {
-    client := greetv1connect.NewServiceClient(
-        http.DefaultClient,
-        "http://localhost:8080",
-    )
+	client := greetv1connect.NewServiceClient(
+		http.DefaultClient,
+		"http://localhost:8080",
+	)
 	reqMsg, err := anypb.New(&greetv1.GreetRequest{
-        Name: "John",
-    })
+		Name: "John",
+	})
 	if err != nil {
-        log.Println(err)
-        return
+		log.Println(err)
+		return
 	}
-    res, err := client.Call(
-        context.Background(),
-        connect.NewRequest(reqMsg),
-    )
-    if err != nil {
-        log.Println(err)
-        return
-    }
-    log.Println(res.Msg.GetTypeUrl())
+	res, err := client.Call(
+		context.Background(),
+		connect.NewRequest(reqMsg),
+	)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	log.Println(res.Msg.GetTypeUrl())
 	resMsg := new(greetv1.GreetResponse)
 	err = res.Msg.UnmarshalTo(resMsg)
 	if err != nil {
-        log.Println(err)
-        return
+		log.Println(err)
+		return
 	}
-    log.Println(resMsg.GetGreeting())
+	log.Println(resMsg.GetGreeting())
 }
